e38cabaad19c7565f036c7eefd178334f1a7b17f
bryant00 <bryantpatton@gmail.com>
2020-04-09 10:24:02 -0700

    cv

--- pages/cv.js
+++ pages/cv.js
@@ -11,13 +11,13 @@ class Cv extends React.Component {
       <BaseLayout {...this.props.auth}>
         <BasePage title="Preview of my CV" className="cv-page">
           <Row>
-            <Col md={{size: 8, offset: 2}}>
+            <Col md={{ size: 8, offset: 2 }}>
               <div className="cv-title">
-                <a download="jerga_cv.pdf" className="btn btn-success" href="/static/jerga_cv.pdf">
+                <a download="bryant_cv.pdf" className="btn btn-success" href="/static/bryant_cv.pdf">
                   Download
                 </a>
               </div>
-              <iframe style={{width: '100%', height: '800px'}} src="/static/jerga_cv.pdf">
+              <iframe style={{ width: '100%', height: '800px' }} src="/static/bryant_cv.pdf">
               </iframe>
             </Col>
           </Row>
