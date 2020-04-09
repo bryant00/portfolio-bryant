a64a7c000d3de3a5c65698b3a57ee81367aff241
bryant00 <bryantpatton@gmail.com>
2020-04-09 09:53:03 -0700

    switch to zeit

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://portfolio-bryant.herokuapp.com' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://portfolio-bryant.herokuapp.com',
+  'process.env.BASE_URL': prod ? 'https://portfolio-bryant.now.sh' : 'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://portfolio-bryant.now.sh',
   'process.env.CLIENT_ID': 'NN4U9SfvteIISTjxY61LxdD2QJns6kA5'
 }
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
   // DB_URI: 'mongodb+srv://myportfolio:BvAukGgF1SWlAQdX@cluster0-rfglz.mongodb.net/test',
-  NAMESPACE: 'https://portfolio-bryant.herokuapp.com'
+  NAMESPACE: 'https://portfolio-bryant.now.sh'
 }
 
